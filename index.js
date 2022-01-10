f9a53f54c4ad806ee0d26b2336b25ec868880e7f
Peter Carpenter <peterc1731@gmail.com>
2022-01-10 09:55:52 +0000

    feat: add bugsnag

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import Bugsnag from '@bugsnag/react-native';
+Bugsnag.start();
+
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
